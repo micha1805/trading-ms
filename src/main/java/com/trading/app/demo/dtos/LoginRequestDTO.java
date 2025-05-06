ff1310a55627b50d3a7fd102a6d60fb4fa5d9129
Micha <michel.plainchamp@gmail.com>
2025-05-06 23:09:31 +0200

    TM-16 - Changed login GET to POST (LoginRequestDTO was buggy)

--- src/main/java/com/trading/app/demo/dtos/LoginRequestDTO.java
+++ src/main/java/com/trading/app/demo/dtos/LoginRequestDTO.java
@@ -2,13 +2,18 @@ package com.trading.app.demo.dtos;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class LoginRequestDTO {
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
     private String email;
+    
+    @NotBlank(message = "Password is required")
     private String password;
-
-    public LoginRequestDTO(@NotBlank(message = "Email is required") @Email(message = "Invalid email format") String email, @NotBlank(message = "Password is required") String password) {
-    }
 }
